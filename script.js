796f6c4e7646f5ffb2e5aca6b03b2d740827cf1f
RushoBinnabi <RushoBinnabi123@yahoo.com>
2023-12-11 17:04:27 -0500

    updated the layout of the recipe pages to be more interactive

--- script.js
+++ script.js
@@ -9,3 +9,59 @@ function homepage() {
 function gitHub() {
     location.href='https://github.com/RushoBinnabi';
 }
+
+function omeletRecipe() {
+    location.href='omelet.html';
+}
+
+function crispyChickenRamenRecipe() {
+    location.href='crispyChickenRamen.html';
+}
+
+function bananaBreadRecipe() {
+    location.href='bananaBread.html';
+}
+
+function whiteBreadRecipe() {
+    location.href='whiteBread.html';
+}
+
+function potatoCakesRecipe() {
+    location.href='potatoCakes.html';
+}
+
+function stirFriedRiceRecipe() {
+    location.href='stir-FriedRice.html';
+}
+
+function breakfastSandwichRecipe() {
+    location.href='breakfastSandwich.html';
+}
+
+function chiliOilRecipe() {
+    location.href='chiliOil.html';
+}
+
+function breakfastBurritosRecipe() {
+    location.href='breakfastBurritos.html';
+}
+
+function bananaBreadPictures() {
+    location.href='bananaBreadPictures.html';
+}
+
+function breakfastBurritosPictures() {
+    location.href='breakfastBurritosPictures.html';
+}
+
+function breakfastSandwichPictures() {
+    location.href='breakfastSandwichPictures.html';
+}
+
+function chiliOilPictures() {
+    location.href='chiliOilPictures.html';
+}
+
+function crispyChickenRamenPictures() {
+    location.href='crispyChickenRamenPictures.html';
+}
