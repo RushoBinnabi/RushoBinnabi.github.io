32b248dfe73e6d6d95ee64b1c675f6226969ec70
RushoBinnabi <RushoBinnabi123@yahoo.com>
2023-12-14 17:01:53 -0500

    added documentation to html files

--- script.js
+++ script.js
@@ -1,82 +1,169 @@
+/*
+    Name: Rusho Binnabi
+    Date: 12/13/2023
+    Project: Website - JavaScript file
+ */
+
+// this JavaScript file handles all the redirects for the website.
+
+/*
+    this recipes() function redirects the webpage to the recipes html file when the appropriate button is pressed.
+ */
 function recipes() {
    location.href='recipes.html';
 }
 
+/*
+    this homepage() function redirects the webpage to the homepage html file when the appropriate button is clicked.
+ */
 function homepage() {
     location.href='index.html';
 }
 
+/*
+    this gitHub() function redirects the webpage to the gitHub html file when the appropriate button is clicked.
+ */
 function gitHub() {
     location.href='https://github.com/RushoBinnabi';
 }
+
+/*
+    this omeletRecipe() function redirects the webpage to the omeletRecipe html file when the appropriate button is clicked.
+ */
 
 function omeletRecipe() {
     location.href='omelet.html';
 }
 
+/*
+    this crispyChickenRamenRecipe() function redirects the webpage to the crispyChickenRamenRecipe html file when the appropriate button is clicked.
+ */
+
 function crispyChickenRamenRecipe() {
     location.href='crispyChickenRamen.html';
 }
+
+/*
+    this bananaBreadRecipe() function redirects the webpage to the bananaBreadRecipe html file when the appropriate button is clicked.
+ */
 
 function bananaBreadRecipe() {
     location.href='bananaBread.html';
 }
 
+/*
+    this whiteBreadRecipe() function redirects the webpage to the whiteBreadRecipe html file when the appropriate button is clicked.
+ */
+
 function whiteBreadRecipe() {
     location.href='whiteBread.html';
 }
+
+/*
+    this potatoCakesRecipe() function redirects the webpage to the potatoCakesRecipe html file when the appropriate button is clicked.
+ */
 
 function potatoCakesRecipe() {
     location.href='potatoCakes.html';
 }
 
+/*
+    this stirFriedRiceRecipe() function redirects the webpage to the stirFriedRiceRecipe html file when the appropriate button is clicked.
+ */
+
 function stirFriedRiceRecipe() {
     location.href='stir-FriedRice.html';
 }
+
+/*
+    this breakfastSandwichRecipe() function redirects the webpage to the breakfastSandwichRecipe html file when the appropriate button is clicked.
+ */
 
 function breakfastSandwichRecipe() {
     location.href='breakfastSandwich.html';
 }
 
+/*
+    this chiliOilRecipe() function redirects the webpage to the chiliOilRecipe html file when the appropriate button is clicked.
+ */
+
 function chiliOilRecipe() {
     location.href='chiliOil.html';
 }
+
+/*
+    this breakfastBurritosRecipe() function redirects the webpage to the breakfastBurritosRecipe html file when the appropriate button is clicked.
+ */
 
 function breakfastBurritosRecipe() {
     location.href='breakfastBurritos.html';
 }
 
+/*
+    this bananaBreadRecipe() function redirects the webpage to the bananaBreadRecipe html file when the appropriate button is clicked.
+ */
 function bananaBreadPictures() {
     location.href='bananaBreadPictures.html';
 }
+
+/*
+    this breakfastBurritosPictures() function redirects the webpage to the breakfastBurritosPictures html file when the appropriate button is clicked.
+ */
 
 function breakfastBurritosPictures() {
     location.href='breakfastBurritosPictures.html';
 }
 
+/*
+    this breakfastSandwichPictures() function redirects the webpage to the breakfastSandwichPictures html file when the appropriate button is clicked.
+ */
 function breakfastSandwichPictures() {
     location.href='breakfastSandwichPictures.html';
 }
+
+/*
+    this chiliOilPictures() function redirects the webpage to the chiliOilPictures html file when the appropriate button is clicked.
+ */
 
 function chiliOilPictures() {
     location.href='chiliOilPictures.html';
 }
 
+/*
+    this crispyChickenRamenPictures() function redirects the webpage to the crispyChickenRamenPictures html file when the appropriate button is clicked.
+ */
+
 function crispyChickenRamenPictures() {
     location.href='crispyChickenRamenPictures.html';
 }
+
+/*
+    this omeletPictures() function redirects the webpage to the omeletPictures html file when the appropriate button is clicked.
+ */
 
 function omeletPictures() {
     location.href='omeletPictures.html';
 }
 
+/*
+    this potatoCakesPictures() function redirects the webpage to the potatoCakesPictures html file when the appropriate button is clicked.
+ */
+
 function potatoCakesPictures() {
     location.href='potatoCakesPictures.html';
 }
 
+/*
+    this stirFriedRicePictures() function redirects the webpage to the stirFriedRicePictures html file when the appropriate button is clicked.
+ */
+
 function stirFriedRicePictures() {
     location.href='stir-FriedRicePictures.html';
 }
+
+/*
+    this whiteBreadPictures() function redirects the webpage to the whiteBreadPictures html file when the appropriate button is clicked.
+ */
 
 function whiteBreadPictures() {
     location.href='whiteBreadPictures.html';
