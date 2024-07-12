3dfbb0260165774798dd62913be7a13fe287776d
Rusho Binnabi <RushoBinnabi123@yahoo.com>
2024-07-12 18:53:37 -0400

    updated navbar, added a new page, and cleaned up some code.

--- script.js
+++ script.js
@@ -7,11 +7,11 @@
 // this JavaScript file handles all the redirects for the website.
 
 /*
-    this recipes() function redirects the webpage to the recipes html file when the appropriate button is pressed.
+    this goToRecipesPage1() function redirects the webpage to the goToRecipesPage1 html file when the appropriate button is pressed.
     And when the page is being redirected, it asks the user to confirm their actions. If they say yes, it redirects the
-    webpage to the recipes html file and if they say no, it stays on the current page.
+    webpage to the goToRecipesPage1 html file and if they say no, it stays on the current page.
  */
-function recipes(event) {
+function goToRecipesPage1(event) {
     let change = confirm("Recipe checklist may not be saved. Continue?");
     if (change) {
         location.href='recipesPage1.html';
@@ -47,17 +47,9 @@ function printPage() {
 /*
     this homepage2() function redirects the webpage to the homepage html file when the appropriate button is clicked.
     This is separate from the homepage() function because this function is used when the user wants to redirect to the
-    homepage from either the recipes or about html file.
+    homepage from either the goToRecipesPage1 or about html file.
  */
 
 function homepage2() {
     location.href='../../index.html';
-}
-
-/*
-    this goToRecipes1stPage() function redirects the webpage to the 1st page of recipes.
- */
-
-function goToRecipes1stPage() {
-    location.href='recipesPage1.html';
 }
